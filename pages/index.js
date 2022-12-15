dabd5ff7af03fcdb6f4a976bb763321c1587ac37
Vitaly Kryanin <krianinvv@internet.ru>
2022-12-15 21:13:18 +0300

    fixed

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ let popup__closeForm = document.querySelector('.popup__close-form');
 let popup = document.querySelector('.popup');
 let profile__buttonEdit = document.querySelector('.profile__button-edit');
 let profile__title = document.querySelector('.profile__title');
+let profile__subtitle = document.querySelector('.profile__subtitle')
+let popup__inputInfo = document.querySelector('.popup__input-info')
 let popup__inputName = document.querySelector('.popup__input-name')
 let popup__sumbitButton = document.querySelector('.popup__sumbit-button')
 
 function render() {
     profile__title.textContent = popup__inputName.value;
+    profile__subtitle.textContent = popup__inputInfo.value;
+    popup.classList.toggle('hidden');
+    root.classList.toggle('block');
 }
 
 function closeForm () {
